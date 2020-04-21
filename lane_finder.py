c7ba911072e9d2252e6bdf9e5d9a5fb57c3836ad
Charanpreet Singh <joyagam@gmail.com>
2020-04-21 16:47:18 +0530

    accomplished single lane markings on both sides on the first test image

--- lane_finder.py
+++ lane_finder.py
@@ -5,9 +5,11 @@ def nothing(x):
     return None
 
 cv.namedWindow('Parameter Tuning')
-cv.createTrackbar('gaussBlur', 'Parameter Tuning', 1 , 29, nothing)
-cv.createTrackbar('lowerThresh', 'Parameter Tuning', 150 , 500, nothing)
-cv.createTrackbar('upperThresh', 'Parameter Tuning', 300 , 500, nothing)
+
+cv.createTrackbar('rho', 'Parameter Tuning', 1 , 30, nothing)
+cv.createTrackbar('min_line_length', 'Parameter Tuning', 1 , 200, nothing)
+cv.createTrackbar('max_line_gap', 'Parameter Tuning', 1 , 200, nothing)
+cv.createTrackbar('threshold', 'Parameter Tuning', 1 , 50, nothing)
 
 lanes = cv.imread('/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/Suppli/test_images/whiteCarLaneSwitch.jpg')
 print(lanes.shape)
@@ -15,19 +17,46 @@ grey = cv.cvtColor(lanes, cv.COLOR_BGR2GRAY)
 
 while True:
 
-    gb = cv.getTrackbarPos('gaussBlur', 'Parameter Tuning')
-    lt = cv.getTrackbarPos('lowerThresh', 'Parameter Tuning')
-    ut = cv.getTrackbarPos('upperThresh', 'Parameter Tuning')
+    #rho = cv.getTrackbarPos('rho', 'Parameter Tuning')
+    #min_line_length = cv.getTrackbarPos('min_line_length', 'Parameter Tuning')
+    #max_line_gap = cv.getTrackbarPos('max_line_gap', 'Parameter Tuning')
+    #threshold = cv.getTrackbarPos('threshold', 'Parameter Tuning')
 
-    gaussBlur = cv.GaussianBlur(grey, (1,1), 0)
-    edges = cv.Canny(gaussBlur, 150, 300)
+    kernel = 1
+    gaussBlur = cv.GaussianBlur(grey, (kernel,kernel), 0)
+    
+    lowerThreshold = 150
+    upperThreshold = 300
+    edges = cv.Canny(gaussBlur, lowerThreshold, upperThreshold)
 
-    quad = np.array([[[110,539],[450,325], [525,325] ,[940,539]]], np.int32)
+    mask = np.zeros_like(edges)
+    vertices = np.array([[(115,539),(450,325), (525,325), (940,539)]], np.int32)
+    #cv.polylines(lanes, [quad], True, (0,255,0), thickness=3)
+    cv.fillPoly(mask, vertices, 255)
+    roi = cv.bitwise_and(mask, edges)
+    
+    rho = 1 
+    theta = np.pi/180 
+    threshold = 20   
+    min_line_length = 0 
+    max_line_gap = 95  
+    lane_marked = lanes.copy()
+    lines = cv.HoughLinesP(roi, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
 
-    cv.polylines(lanes, [quad], True, (0,255,0), thickness=3)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv.line(lane_marked,(x1,y1),(x2,y2),(255,0,0),10)
 
+# Create a "color" binary image to combine with line image
+    #color_edges = np.dstack((edges, edges, edges)) 
+    original = lanes.copy()
+# Draw the lines on the edge image
+    lines_edges = cv.addWeighted(lane_marked, 0.5, original, 0.5, 0) 
+    
     cv.imshow('edges', edges)
     cv.imshow('lane', lanes)
+    cv.imshow('roi', roi)
+    cv.imshow('Lanes Marked', lines_edges)
 
     k = cv.waitKey(1) & 0xFF
     if k == 27:
