95ad8e1d6f1189f6a27e6cc93be89a21bb6eb5ff
Charanpreet Singh <joyagam@gmail.com>
2020-04-26 20:05:34 +0530

    all done

--- lane_finder.py
+++ lane_finder.py
@@ -1,7 +1,13 @@
 import cv2 as cv
 import numpy as np
 from math import sqrt
+from statistics import mean
 import os
+
+right_slope_avg = 0
+right_intercept_avg = 0
+left_slope_avg = 0
+left_intercept_avg = 0
 
 # Use to easily visualize the tuninng of paramters.
 
@@ -31,53 +37,120 @@ def trackbarPos():
     
     #Creates ROI 
 def ROI(image):
-    mask = np.zeros_like(image) # creating a black image of size provided as argument
-    vertices = np.array([[(100,539),(420,335), (540,335), (940,539)]], np.int32) # vertices of the polygon for roi
-    #cv.polylines(edges, vertices, True, (0,255,0), thickness=3) # for visualization of polygon
-    cv.fillPoly(mask, vertices, 255) # filling the roi with white pixels in efforts to extract the particular region
     global roi
-    roi = cv.bitwise_and(mask, image) # AND operation with the canny edge image
+    global height_img
+    global res_y
+    global lanes
+    height_img = image.shape[0]
+    
+    # Different ROI for the challenge video as it has different dimensions
+    if height_img == 720:
+        
+        lower_y = np.array([0, 0, 220]) # Defining ranges in HSV to isolate yellow and white color
+        upper_y = np.array([255, 255, 255])
+        lanes_hsv = cv.cvtColor(lanes, cv.COLOR_BGR2HSV) # Converting BGR image to HSV
+        mask_y = cv.inRange(lanes_hsv, lower_y, upper_y) # Forming a mask
+        res_y = cv.bitwise_and(lanes, lanes, mask=mask_y) # ANDing the mask with the original image 
+        res_gray = cv.cvtColor(res_y, cv.COLOR_BGR2GRAY)
+        image = cv.bitwise_or(res_gray, image) # ORing Canny edge image and HSV image for better line detection
+        
+        mask = np.zeros_like(image) # creating a black image of size provided as argument
+        vertices = np.array([[(150,719),(500,490), (820,490), (1160,719)]], np.int32) # vertices of the polygon for roi
+        #cv.polylines(edges, vertices, True, (0,255,0), thickness=3) # for visualization of polygon
+        cv.fillPoly(mask, vertices, 255) # filling the roi with white pixels in efforts to extract the particular region
+        roi = cv.bitwise_and(mask, image) # AND operation with the canny edge image
+    
+    else:
+        
+        mask = np.zeros_like(image) # creating a black image of size provided as argument
+        vertices = np.array([[(100,539),(420,335), (540,335), (940,539)]], np.int32) # vertices of the polygon for roi
+        #cv.polylines(edges, vertices, True, (0,255,0), thickness=3) # for visualization of polygon
+        cv.fillPoly(mask, vertices, 255) # filling the roi with white pixels in efforts to extract the particular region
+        roi = cv.bitwise_and(mask, image) # AND operation with the canny edge image
 
 #Draws single Lane Lines from bottom of roi to the top without any gaps on both sides 
 def drawLines():
-    rho = 15 # distance from..
-    theta = np.pi/180 # angle..
-    threshold = 80 # Number of intersecting lines in hough space, these lines are pts in original space that lie on the same line
-    min_line_length = 200 # length in pixels required to be considered as a line 
-    max_line_gap = 200 # maximum gap in pixels between subsequent line segments for them to be considered as one line
+    rho = 1 # distance from the coordinate origin
+    theta = np.pi/180 # angle in radians in the polar coordinate system
+    threshold = 45 # Number of intersecting lines in hough space, these lines are pts in original space that lie on the same line
+    min_line_length = 40 # length in pixels required to be considered as a line 
+    max_line_gap = 120 # maximum gap in pixels between subsequent line segments for them to be considered as one line
     global lane_marked
+    global right_slope_avg 
+    global right_intercept_avg 
+    global left_slope_avg 
+    global left_intercept_avg
     lane_marked = lanes.copy() # copy of original image for creating lines
-    lines = cv.HoughLinesP(roi, rho, theta, threshold, np.array([]), min_line_length, max_line_gap) # Houghline transform returns the lines 
-    i = 0
-    lengths_plus = []
-    coordinates_plus =  []
-    lengths_minus = []
-    coordinates_minus =  []
+    lines = cv.HoughLinesP(roi, rho, theta, threshold, np.array([]), min_line_length, max_line_gap) # Houghline transform returns the line segments  
+    right_slope = []
+    right_intercept = []
+    left_slope = []
+    left_intercept = []
     
-    # Drawing the detected lines 
+    #Lane line starting and ending pts(ROI top and bottom)
+    if height_img==720:
+        y_top = 490
+        y_bottom = 719
+    else:
+        y_top = 335
+        y_bottom = 539
+    
+    #Using line segments predicted by hough transform
     for line in lines:
-
         for x1,y1,x2,y2 in line:
-            length = sqrt(((x2-x1)**2 + (y2-y1)**2))
-            slope = (y2-y1)/(x2-x1)
-            if slope>0:
-                lengths_plus.append(length)
-                coordinates_plus.append([x1,y1,x2,y2])
-            else:
-                lengths_minus.append(length)
-                coordinates_minus.append([x1,y1,x2,y2])
+            slope = (y2-y1)/(x2-x1) # calculating slope of all lines
+            intercept = y1 - (slope*x1) # intercept
             
-            #print('line', i,  " slope = ",  slope, ' length = ', length)
-        i = i + 1    
+            # Getting rid of outliers
+            if slope>=0 and (slope<=0.2 or slope>=0.89):
+                continue
+            elif slope<0 and (slope>-0.2 or slope<-0.89):
+                continue
+                
+            # Differentiating between left and right lane slopes
+            if slope>0:
+                left_intercept.append(intercept)
+                left_slope.append(slope)
+            else:
+                right_intercept.append(intercept)
+                right_slope.append(slope)
+            
+    # For right lane
+    
+    # When no line segments detected using previous results
+    while len(right_intercept)==0:
+        right_intercept.append(right_intercept_avg)
+    while len(right_slope)==0:
+        right_slope.append(right_slope_avg)
+    
+    # Using Average values for smoother results
+    right_intercept_avg = mean(right_intercept)
+    right_slope_avg = mean(right_slope)
+    
+    # Using line equation to find the X axis values for the lines as y value is already set to roi top and bottom
+    xtop_right = (y_top - right_intercept_avg)/right_slope_avg
+    xbottom_right = (y_bottom - right_intercept_avg)/right_slope_avg
 
-    ind_plus = lengths_plus.index(max(lengths_plus))
-    coor_plus = coordinates_plus[ind_plus]
-    cv.line(lane_marked,(coor_plus[0],coor_plus[1]),(coor_plus[2],coor_plus[3]),(255,0,0),15)
-
-    ind_minus = lengths_minus.index(max(lengths_minus))
-    coor_minus = coordinates_minus[ind_minus]
-    cv.line(lane_marked,(coor_minus[0],coor_minus[1]),(coor_minus[2],coor_minus[3]),(255,0,0),15)
-
+    # for left lane
+    
+    # When no line segments detected using previous results
+    while len(left_intercept)==0:
+        left_intercept.append(left_intercept_avg)
+    while len(left_slope)==0:
+        left_slope.append(left_slope_avg)
+    
+    # Using Average values for smoother results
+    left_intercept_avg = mean(left_intercept)
+    left_slope_avg = mean(left_slope)
+    
+    # Using line equation to find the X axis values for the lines as y value is already set to roi top and bottom
+    xtop_left = (y_top - left_intercept_avg)/left_slope_avg
+    xbottom_left = (y_bottom - left_intercept_avg)/left_slope_avg 
+        
+    # drawing lines
+    cv.line(lane_marked, (int(xtop_left), y_top), (int(xbottom_left), y_bottom), (255,0,0), 10)
+    cv.line(lane_marked, (int(xtop_right), y_top), (int(xbottom_right), y_bottom), (255,0,0), 10)    
+    
 # Adds original image and lane marked image to lower the intensity of the line marking
 def reduceLineIntensity():
     original = lanes.copy()
@@ -88,36 +161,47 @@ def reduceLineIntensity():
 def show():
     #cv.imshow('edges', edges)
     #cv.imshow('lane', lanes)
-    #cv.imshow('roi', roi)
+    cv.imshow('roi', roi)
     cv.imshow('Lanes Marked', lane_blended)
     
  
 # ___________________________________Main--Function___________________________________________
 
 # Input of image or video file
-path_folder = '/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/test_images'
-for files in os.listdir(path_folder):
+#path_folder = '/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/test_images'
+#for files in os.listdir(path_folder):
     
-    lanes = cv.imread(path_folder + "/" + files)
-    #print(lanes.shape)
+#lanes = cv.imread(path_folder + "/" + files)
+#print(lanes.shape)
 
-    #parameterTuning()
-    #Converting to grayscale for edge detection
-    grey = cv.cvtColor(lanes, cv.COLOR_BGR2GRAY)
+#parameterTuning()
+#Converting to grayscale for edge detection
 
-    while True:
+vid = cv.VideoCapture('/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/test_videos/challenge.mp4')
 
-        #trackbarPos()
+frame_width = int(vid.get(3))
+frame_height = int(vid.get(4))
+out = cv.VideoWriter('/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/test_videos_output/challenge_output.avi', cv.VideoWriter_fourcc('M','J','P','G'), 30, (frame_width,frame_height))
+
+while (vid.isOpened()):
+    
+    ret, lanes = vid.read()
+    #trackbarPos()
+    if ret == True:
+        grey = cv.cvtColor(lanes, cv.COLOR_BGR2GRAY)
 
         #Blurring image for better edge detection
         kernel = 1 # kernel size for blurring operation
         gaussBlur = cv.GaussianBlur(grey, (kernel,kernel), 0)
         
         #Best threshold values found after tuning on multiple test images.
-        lowerThreshold = 100 # below which..
-        upperThreshold = 200 # above which..
-        cv.line(gaussBlur,(25,539),(225,539),(255,0,0),5)
-        cv.line(gaussBlur,(900,539),(700,539),(255,0,0),5)
+        if gaussBlur.shape[0] ==720:
+            lowerThreshold = 400 # below which..
+            upperThreshold = 500 # above which..
+        else:
+            lowerThreshold = 100 # below which..
+            upperThreshold = 300 # above which..
+        
         edges = cv.Canny(gaussBlur, lowerThreshold, upperThreshold)
 
         ROI(edges)
@@ -127,11 +211,14 @@ for files in os.listdir(path_folder):
         reduceLineIntensity()
 
         show()
+        out.write(lane_blended)
 
     #cv.imwrite('/home/blackpanther/Desktop/Projects/sdr_projects/lane_finder/test_images_output/solidWhiteCurve_output.jpg', lane_blended)
     # Exiting the loop
-        k = cv.waitKey(1) & 0xFF
+        k = cv.waitKey(25) & 0xFF
         if k == 27:
             break
-
-    cv.destroyAllWindows()
+    else:
+        break
+vid.release()
+cv.destroyAllWindows()
